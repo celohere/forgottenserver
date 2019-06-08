7ba949d95230344a06704a8347e1f4a21d198bf4
Emilianenko <celohere@gmail.com>
2019-06-08 14:52:51 -0300

    Update configmanager.h

--- src/configmanager.h
+++ src/configmanager.h
@@ -47,7 +47,8 @@ class ConfigManager
 			CLASSIC_ATTACK_SPEED,
 			AUTO_STACK_ITEMS,
 			LOOT_MESSAGE,
-
+                        REMOVE_WEAPON_AMMO,
+			
 			LAST_BOOLEAN_CONFIG /* this must be the last one */
 		};
 
