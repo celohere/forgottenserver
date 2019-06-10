3ed5a2adee24e95ce5bf4de49bfc48f3d4a33845
celohere <41874679+celohere@users.noreply.github.com>
2019-06-10 19:52:44 -0300

    Create 19.lua

---
+++ data/migrations/19.lua
@@ -0,0 +1,7 @@
+function onUpdateDatabase()
+    print("> Updating database to version 19 (player_bans)")
+
+    db.query("CREATE TABLE IF NOT EXISTS `player_bans` (`id` INT UNSIGNED NOT NULL AUTO_INCREMENT, `player_id` int(11) NOT NULL, `reason` varchar(255) NOT NULL, `banned_at` bigint(20) NOT NULL, `expires_at` bigint(20) NOT NULL, `banned_by` int(11) NOT NULL, PRIMARY KEY (`id`), KEY (`player_id`), KEY `banned_by` (`banned_by`), FOREIGN KEY (`player_id`) REFERENCES `players` (`id`) ON DELETE CASCADE ON UPDATE CASCADE, FOREIGN KEY (`banned_by`) REFERENCES `players` (`id`) ON DELETE CASCADE ON UPDATE CASCADE) ENGINE=InnoDB")
+
+    return true
+end
