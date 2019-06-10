f5d15ea907cae06d6c3a7da24b9723cd25ec5332
celohere <41874679+celohere@users.noreply.github.com>
2019-06-10 19:51:04 -0300

    Update playerdeath.lua

--- data/creaturescripts/scripts/playerdeath.lua
+++ data/creaturescripts/scripts/playerdeath.lua
@@ -1,6 +1,6 @@
 local deathListEnabled = true
 local maxDeathRecords = 5
-local killsToBan = 10
+local killsToBan = 6
 local killsBanLenght = 2
 
 function onDeath(player, corpse, killer, mostDamageKiller, unjustified, mostDamageUnjustified)
@@ -90,7 +90,7 @@ function onDeath(player, corpse, killer, mostDamageKiller, unjustified, mostDama
 			local kills = math.ceil(skullTime / configManager.getNumber(configKeys.FRAG_TIME))
 			if kills >= killsToBan then
 				local timeNow = os.time()
-				db.query("INSERT INTO `account_bans` (`account_id`, `reason`, `banned_at`, `expires_at`, `banned_by`) VALUES (" .. killer:getAccountId() .. ", " .. db.escapeString("Excessive Unjustified Player Killing.") .. ", " .. timeNow .. ", " .. timeNow + (killsBanLenght * 86400) .. ", 1)")
+                                db.query("INSERT INTO `player_bans ` (`player_id`, `reason`, `banned_at`, `expires_at`, `banned_by`) VALUES (" .. killer:getGuid() .. ", " .. db.escapeString("Excessive Unjustified Player Killing.") .. ", " .. timeNow .. ", " .. timeNow + (killsBanLenght * 86400) .. ", 1)")
 				killer:getPosition():sendMagicEffect(CONST_ME_MAGIC_RED)
 				killer:remove()
 			end
