f59d0ccb4ed56cd99479b778b3af47e54558e235
celohere <41874679+celohere@users.noreply.github.com>
2019-07-25 21:05:31 -0300

    revert last commit

--- src/talkaction.cpp
+++ src/talkaction.cpp
@@ -64,8 +64,7 @@ Event* TalkActions::getEvent(const std::string& nodeName)
 
 bool TalkActions::registerEvent(Event* event, const pugi::xml_node&)
 {
-        auto talkAction = std::unique_ptr<TalkAction>(static_cast<TalkAction*>(event)); // event is guaranteed to be a TalkAction
-	talkActions.push_front(std::move(*talkAction));
+	talkActions.push_front(static_cast<TalkAction*>(event)); // event is guaranteed to be a TalkAction
 	return true;
 }
 
