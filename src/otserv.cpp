ce3da7ddbd52a2f0e447019828e030a94a628d9d
celohere <41874679+celohere@users.noreply.github.com>
2019-11-29 20:21:37 -0300

    Update otserv.cpp

--- src/otserv.cpp
+++ src/otserv.cpp
@@ -72,16 +72,6 @@ int main(int argc, char* argv[])
 {
 	// Setup bad allocation handler
 	std::set_new_handler(badAllocationHandler);
-
-#ifndef _WIN32
-	// ignore sigpipe...
-	struct sigaction sigh;
-	sigh.sa_handler = SIG_IGN;
-	sigh.sa_flags = 0;
-	sigemptyset(&sigh.sa_mask);
-	sigaction(SIGPIPE, &sigh, nullptr);
-#endif
-
 	ServiceManager serviceManager;
 
 	g_dispatcher.start();
@@ -93,19 +83,6 @@ int main(int argc, char* argv[])
 
 	if (serviceManager.is_running()) {
 		std::cout << ">> " << g_config.getString(ConfigManager::SERVER_NAME) << " Server Online!" << std::endl << std::endl;
-#ifdef _WIN32
-		SetConsoleCtrlHandler([](DWORD) -> BOOL {
-			g_dispatcher.addTask(createTask([]() {
-				g_dispatcher.addTask(createTask(
-					std::bind(&Game::shutdown, &g_game)
-				));
-				g_scheduler.stop();
-				g_databaseTasks.stop();
-				g_dispatcher.stop();
-			}));
-			ExitThread(0);
-		}, 1);
-#endif
 		serviceManager.run();
 	} else {
 		std::cout << ">> No services running. The server is NOT online." << std::endl;
@@ -192,6 +169,13 @@ void mainLoader(int, char*[], ServiceManager* services)
 
 	if (g_config.getBoolean(ConfigManager::OPTIMIZE_DATABASE) && !DatabaseManager::optimizeTables()) {
 		std::cout << "> No tables were optimized." << std::endl;
+	}
+	
+	//load groups
+	std::cout << ">> Loading groups" << std::endl;
+	if (!g_game.groups.load()) {
+		startupErrorMessage("Unable to load groups!");
+		return;
 	}
 
 	//load vocations
