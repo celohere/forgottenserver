bc8def8433794452402bc1cbcd5c00a7416844a0
celohere <41874679+celohere@users.noreply.github.com>
2019-12-07 22:46:26 -0300

    Update rope.lua
    Fixed small error and update script.

--- data/actions/scripts/tools/rope.lua
+++ data/actions/scripts/tools/rope.lua
@@ -4,27 +4,41 @@ local holeId = {
 }
 
 function onUse(player, item, fromPosition, target, toPosition, isHotkey)
-    local tile = Tile(toPosition)
-    if not tile then
-        return false
-    end
+	local tile = Tile(toPosition)
+	if not tile then
+		return false
+	end
+	
+	if not tile:getGround() then
+		return false
+	end
 
-    if isInArray(ropeSpots, tile:getGround():getId()) then
-        player:teleportTo(toPosition:moveUpstairs(), false)
-        return true
-    elseif isInArray(holeId, target.itemid) then
-        toPosition.z = toPosition.z + 1
-        tile = Tile(toPosition)
-        if tile then
-            local thing = tile:getTopVisibleThing()
-            if thing:isItem() and thing:getType():isMovable() then
-                return thing:moveTo(toPosition:moveUpstairs())
-            elseif thing:isCreature() then
+	if table.contains(ropeSpots, tile:getGround():getId()) then
+		if Tile(toPosition:moveUpstairs()):hasFlag(TILESTATE_PROTECTIONZONE) and player:isPzLocked() then
+			player:sendCancelMessage(RETURNVALUE_PLAYERISPZLOCKED)
+			return true
+		end
+		player:teleportTo(toPosition, false)
+		return true
+	elseif table.contains(holeId, target.itemid) then
+		toPosition.z = toPosition.z + 1
+		tile = Tile(toPosition)
+		if tile then
+			local thing = tile:getTopVisibleThing()
+			if thing:isPlayer() then
+				if Tile(toPosition:moveUpstairs()):hasFlag(TILESTATE_PROTECTIONZONE) and thing:isPzLocked() then
+					return false
+				end
+				return thing:teleportTo(toPosition, false)
+			end
+			if thing:isItem() and thing:getType():isMovable() then
+				return thing:moveTo(toPosition:moveUpstairs())
+			elseif thing:isCreature() then
                 return thing:teleportTo(toPosition:moveUpstairs())
-            end
-        end
-        player:sendCancelMessage(RETURNVALUE_NOTPOSSIBLE)
-        return true
-    end
-    return false
+			end
+		end
+		player:sendCancelMessage(RETURNVALUE_NOTPOSSIBLE)
+		return true
+	end
+	return false
 end
