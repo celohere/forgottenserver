9c51be37468809f64f568a2bda304c6926b6cd69
celohere <41874679+celohere@users.noreply.github.com>
2019-12-26 17:56:59 -0300

    Update 20.lua

--- data/migrations/20.lua
+++ data/migrations/20.lua
@@ -1,3 +1,7 @@
 function onUpdateDatabase()
-	return false
+    print("> Updating database to version 21 (server_record)")
+	
+	    db.query("CREATE TABLE IF NOT EXISTS `server_record` (`record` INT NOT NULL, `timestamp` BIGINT NOT NULL, UNIQUE (`record`, `timestamp`)) ENGINE = InnoDB;")
+		
+    return true
 end
