91052c41fb962c44eafa94e62f27e8264aed908f
celohere <41874679+celohere@users.noreply.github.com>
2019-12-26 10:41:36 -0300

    Delete entrancepoi.lua

--- data/actions/scripts/entrancepoi.lua
+++
@@ -1,9 +0,0 @@
-function onUse(cid, item, frompos, item2, topos)
-gatepos = {x=348, y=446, z=8, stackpos=1}
-getgate = getThingfromPos(gatepos)
-if item.uid == 2319 then
-doTeleportThing(cid, gatepos)
-doSendMagicEffect(gatepos, 10)
-end
-return 1
-end
