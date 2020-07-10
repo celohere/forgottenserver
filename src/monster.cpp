d17cb285b11fa8112dfd945ba59b7a644a93311f
celohere <41874679+celohere@users.noreply.github.com>
2020-07-09 21:57:24 -0300

    Update monster.cpp

--- src/monster.cpp
+++ src/monster.cpp
@@ -806,6 +806,10 @@ void Monster::doAttacking(uint32_t interval)
 	for (const spellBlock_t& spellBlock : mType->attackSpells) {
 		bool inRange = false;
 
+		if (attackedCreature == nullptr) {
+			break;
+		}
+
 		if (canUseSpell(myPos, targetPos, spellBlock, interval, inRange, resetTicks)) {
 			if (spellBlock.chance >= static_cast<uint32_t>(uniform_random(1, 100))) {
 				if (updateLook) {
