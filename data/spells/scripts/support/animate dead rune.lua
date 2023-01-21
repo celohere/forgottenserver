dbc1955887d8ff278041079130b16d6413ac38ab
celohere <41874679+celohere@users.noreply.github.com>
2023-01-21 16:57:21 -0300

    Fix animate dead max summons
    Added rune summon limit of 2 skeletons.

--- data/spells/scripts/support/animate dead rune.lua
+++ data/spells/scripts/support/animate dead rune.lua
@@ -1,17 +1,23 @@
 function onCastSpell(creature, variant, isHotkey)
 	local position = Variant.getPosition(variant)
 	local tile = Tile(position)
-	if tile and creature:getSkull() ~= SKULL_BLACK then
+	if tile then
 		local corpse = tile:getTopDownItem()
 		if corpse then
 			local itemType = corpse:getType()
 			if itemType:isCorpse() and itemType:isMovable() then
-				local monster = Game.createMonster("Skeleton", position)
-				if monster then
-					corpse:remove()
-					monster:setMaster(creature)
-					position:sendMagicEffect(CONST_ME_MAGIC_BLUE)
-					return true
+				if #creature:getSummons() < 2 and creature:getSkull() ~= SKULL_BLACK then
+					local monster = Game.createMonster("Skeleton", position)
+					if monster then
+						corpse:remove()
+						monster:setMaster(creature)
+						position:sendMagicEffect(CONST_ME_MAGIC_BLUE)
+						return true
+					end
+				else
+					creature:sendCancelMessage("You cannot control more creatures.")
+					creature:getPosition():sendMagicEffect(CONST_ME_POFF)
+					return false
 				end
 			end
 		end
